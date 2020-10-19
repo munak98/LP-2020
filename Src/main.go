f5dc5b0b4e22de1a504b5bb35c9a90d4900643ba
Manuela Matos <munak98@hotmail.com>
2020-10-19 19:44:45 -0300

    pushing first files

---
+++ Src/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+  "fmt"
+  "encoding/csv"
+  "os"
+  "golang.org/x/text/encoding/charmap"
+  "extract"
+  )
+
+func main() {
+    if len(os.Args) < 2 {
+      fmt.Printf("No input file. Please supply a valid csv file \n");
+      return
+    }
+
+    fmt.Printf("Reading: %s\n", fmt.Sprintf("../../Data/%s", os.Args[1]))
+
+    csv_file, err := os.Open(fmt.Sprintf("../../Data/%s", os.Args[1]))
+    if err != nil { //check for error in opening
+      fmt.Println("An error encountered ::", err)
+    }
+    reader := csv.NewReader(charmap.ISO8859_1.NewDecoder().Reader(csv_file))
+    reader.Comma = ';'
+
+    extract.MeanScoresUF(reader, csv_file, "DF")
+
+    return
+  }
