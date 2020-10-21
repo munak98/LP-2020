031c0349cac0ac23d5582f181f9b076029e987b0
Capelaum <thecapellett@gmail.com>
2020-10-21 01:34:12 -0300

    Total records and time tracking

--- Packages/extract/extract.go
+++ Packages/extract/extract.go
@@ -8,11 +8,16 @@ import (
   "github.com/montanaflynn/stats"
 )
 
-func MeanScoresUF(reader *csv.Reader, file *os.File, UF string) []float64{
+//MeanScoresUF gets means scores from a UF info from a csv file
+func MeanScoresUF(reader *csv.Reader, file *os.File, UF string, finished chan bool) []float64{
   scores := [4][]float64{}
+  totalUFRecords := 0
 
-  for i:= 0 ; i < 10000; i++ { //read file line by line
+  //read file line by line
+  for i := 0; i < 100; i++{ 
+    
     record, err := reader.Read()
+
     if err == io.EOF {
       break   // reached end of the file
     } else if err != nil{ //check for other errors
@@ -24,14 +29,21 @@ func MeanScoresUF(reader *csv.Reader, file *os.File, UF string) []float64{
       scores[1] = append(scores[1], getScoreCH(record))
       scores[2] = append(scores[2], getScoreLC(record))
       scores[3] = append(scores[3], getScoreMT(record))
+
+      totalUFRecords++;
     }
+
   }
+
+  fmt.Printf("Total de registros analisados de %s: %d\n", UF, totalUFRecords)
 
   meanCN, _ := stats.Mean(scores[0])
   meanCH, _ := stats.Mean(scores[1])
   meanLC, _ := stats.Mean(scores[2])
   meanMT, _ := stats.Mean(scores[3])
+
   fmt.Printf("%s médias: \n\tCiências da natureza: %.2f \n\tCiências humanas: %.2f \n\tLinguagens e códigos: %.2f\n\tMatemática: %.2f\n", UF, meanCN, meanCH, meanLC, meanMT)
-  
+	finished <- true
+
   return []float64{meanCN, meanCH, meanLC, meanMT}
 }
--- Src/main.go
+++ Src/main.go
@@ -1,29 +1,50 @@
 package main
 
 import (
-  "fmt"
-  "encoding/csv"
-  "os"
-  "golang.org/x/text/encoding/charmap"
-  "github.com/munak98/LP-2020/Packages/extract"
-  )
+	"encoding/csv"
+	"fmt"
+	"os"
+	"time"
+
+	"../Packages/extract"
+	"golang.org/x/text/encoding/charmap"
+)
+
+func worker(finished chan bool) {
+	fmt.Println("Worker: Started")
+	time.Sleep(time.Second)
+	fmt.Println("Worker: Finished")
+	finished <- true
+}
 
 func main() {
-    if len(os.Args) < 2 {
-      fmt.Printf("No input file. Please supply a valid csv file \n");
-      return
-    }
 
-    fmt.Printf("Reading: %s\n", fmt.Sprintf("../%s", os.Args[1]))
+	now := time.Now()
+	defer func() {
+		fmt.Println(time.Since(now))
+	}()
 
-    csv_file, err := os.Open(fmt.Sprintf("../%s", os.Args[1]))
-    if err != nil { //check for error in opening
-      fmt.Println("An error encountered ::", err)
-    }
-    reader := csv.NewReader(charmap.ISO8859_1.NewDecoder().Reader(csv_file))
-    reader.Comma = ';'
+	if len(os.Args) < 2 {
+		fmt.Printf("No input file. Please supply a valid csv file \n")
+		return
+	}
 
-    extract.MeanScoresUF(reader, csv_file, "DF")
+	fmt.Printf("Reading: %s\n", fmt.Sprintf("../%s", os.Args[1]))
 
-    return
-  }
+	csvFile, err := os.Open(fmt.Sprintf("../%s", os.Args[1]))
+
+	if err != nil { //check for error in opening
+		fmt.Println("An error encountered ::", err)
+	}
+
+	reader := csv.NewReader(charmap.ISO8859_1.NewDecoder().Reader(csvFile))
+  reader.Comma = ';'
+  
+  finished := make(chan bool)
+  
+  go extract.MeanScoresUF(reader, csvFile, "DF", finished)
+
+	<- finished
+
+	return
+}
