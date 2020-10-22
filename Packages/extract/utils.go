9df7a85cb1211ea73524324b2d47847d64ba3dee
Capelaum <thecapellett@gmail.com>
2020-10-22 02:23:16 -0300

    Participantes e Medias por raça
    Participantes e Medias por raça, Mini Menu

--- Packages/extract/utils.go
+++ Packages/extract/utils.go
@@ -7,7 +7,8 @@ import (
 	"golang.org/x/text/encoding/charmap"
 )
 
-//CsvReader Reads the csv File
+
+// CsvReader faz a leitura do arquivo csv 
 func CsvReader() *csv.Reader {
 
 	csvFilePath := "../microdados_enem_2019/DADOS/MICRODADOS_ENEM_2019.csv"
@@ -15,7 +16,7 @@ func CsvReader() *csv.Reader {
 
 	csvFile, err := os.Open(csvFilePath)
 
-	if err != nil { //check for error in opening
+	if err != nil { // chaca se ocorre erros na abertura do csv
 		fmt.Println("An error encountered ::", err)
 	}
 
@@ -23,4 +24,19 @@ func CsvReader() *csv.Reader {
 	reader.Comma = ';'
 
 	return reader
+}
+
+func printMeanScores(topico string, meanScores [4]float64) {
+	fmt.Printf("Médias de %s: \n\tCiências da natureza: %.2f \n\tCiências humanas: %.2f \n\tLinguagens e códigos: %.2f\n\tMatemática: %.2f\n\n", 
+		topico, meanScores[0], meanScores[1], meanScores[2], meanScores[3])
+}
+
+// Contains verifica se existe string em um array de strings
+func Contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
